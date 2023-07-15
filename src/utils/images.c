15c90bf2cbe9708470ad1e6948f90d8c8126649c
Stoupy51 <stoupy51@gmail.com>
2023-07-15 11:43:43 +0200

    rewrote image functions

--- src/utils/images.c
+++ src/utils/images.c
@@ -11,28 +11,27 @@
  * @brief Load an image from a file, fills the width, height and channels variables
  * and returns a pointer to the image data.
  * 
- * @param file_name			Name of the file
- * @param width				Pointer to the width variable
- * @param height			Pointer to the height variable
- * @param channels			Pointer to the channels variable (1 for grayscale, 3 for RGB)
+ * @param file_name		Name of the file
+ * @param image			Pointer to the image structure
  * 
- * @return unsigned char*	Pointer to the image data (must be freed) or NULL if an error occurred
+ * @return int			0 if no error occurred, -1 otherwise
  */
-unsigned char* image_load(const char* file_name, int* width, int* height, int* channels) {
+int image_load(const char* file_name, image_t* image) {
+
+	// Initialize the image structure
+	memset(image, 0, sizeof(image_t));
 
 	// Open the file for reading in binary mode
 	FILE* file = fopen(file_name, "rb");
-	ERROR_HANDLE_PTR_RETURN_NULL(file, "image_load(): Error opening the file '%s'.\n", file_name);
+	ERROR_HANDLE_PTR_RETURN_INT(file, "image_load(): Error opening the file '%s'\n", file_name);
 
 	// Load the image
-	unsigned char* image = stbi_load_from_file(file, width, height, channels, 0);
-	ERROR_HANDLE_PTR_RETURN_NULL(image, "image_load_png(): Error loading the image.\n");
+	image->data = stbi_load_from_file(file, &image->width, &image->height, &image->channels, 0);
+	ERROR_HANDLE_PTR_RETURN_INT(image->data, "image_load(): Error loading the image '%s'\n", file_name);
 
-	// Close the file
+	// Close the file & return
 	fclose(file);
-
-	// Return the image
-	return image;
+	return 0;
 }
 
 /**
@@ -53,21 +52,18 @@ void img_write_func(void* context, void* data, int size) {
  * @brief Save an image to a PNG file.
  * 
  * @param file_name		Name of the file
- * @param image			Pointer to the image data
- * @param width			Width of the image
- * @param height		Height of the image
- * @param channels		Channels of the image (1 for grayscale, 3 for RGB)
+ * @param image			Image to save
  * 
  * @return int			0 if no error occurred, -1 otherwise
  */
-int image_save_png(const char* file_name, unsigned char* image, int width, int height, int channels) {
+int image_save_png(const char* file_name, image_t image) {
 	
 	// Open the file for writing in binary mode
 	FILE* file = fopen(file_name, "wb");
 	ERROR_HANDLE_PTR_RETURN_INT(file, "image_save_png(): Error opening the file '%s'.\n", file_name);
 
 	// Save the image
-	int code = stbi_write_png_to_func(img_write_func, file, width, height, channels, image, 0);
+	int code = stbi_write_png_to_func(img_write_func, file, image.width, image.height, image.channels, image.data, 0);
 	ERROR_HANDLE_INT_RETURN_INT(code, "image_save_png(): Error saving the image.\n");
 
 	// Close the file
@@ -89,14 +85,14 @@ int image_save_png(const char* file_name, unsigned char* image, int width, int h
  * 
  * @return int			0 if no error occurred, -1 otherwise
  */
-int image_save_jpg(const char* file_name, unsigned char* image, int width, int height, int channels, int quality) {
+int image_save_jpg(const char* file_name, image_t image, int quality) {
 	
 	// Open the file for writing in binary mode
 	FILE* file = fopen(file_name, "wb");
 	ERROR_HANDLE_PTR_RETURN_INT(file, "image_save_jpg(): Error opening the file '%s'.\n", file_name);
 
 	// Save the image
-	int code = stbi_write_jpg_to_func(img_write_func, file, width, height, channels, image, quality);
+	int code = stbi_write_jpg_to_func(img_write_func, file, image.width, image.height, image.channels, image.data, quality);
 	ERROR_HANDLE_INT_RETURN_INT(code, "image_save_jpg(): Error saving the image.\n");
 
 	// Close the file
--- src/utils/images.h
+++ src/utils/images.h
@@ -2,10 +2,18 @@
 #ifndef __IMAGES_H__
 #define __IMAGES_H__
 
+//
+typedef struct image_t {
+	int width;
+	int height;
+	int channels;
+	unsigned char* data;
+} image_t;
+
 // Functions
-unsigned char* image_load(const char* file_name, int* width, int* height, int* channels);
-int image_save_png(const char* file_name, unsigned char* image, int width, int height, int channels);
-int image_save_jpg(const char* file_name, unsigned char* image, int width, int height, int channels, int quality);
+int image_load(const char* file_name, image_t* image);
+int image_save_png(const char* file_name, image_t image);
+int image_save_jpg(const char* file_name, image_t image, int quality);
 
 
 #endif
