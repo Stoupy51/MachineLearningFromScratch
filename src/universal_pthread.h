997035438e2925622aa564d26ab3a6396da567f9
Stoupy51 <stoupy51@gmail.com>
2023-07-29 14:59:56 +0200

    universal pthread adjustments

--- src/universal_pthread.h
+++ src/universal_pthread.h
@@ -12,19 +12,21 @@
 	#define thread_param LPVOID
 	#define pthread_t HANDLE
 	#define pthread_create(thread, attr, start_routine, arg) ((*thread = CreateThread(NULL, 0, start_routine, arg, 0, NULL)) == NULL) ? -1 : 0
-	#define pthread_join(thread, value_ptr) (WaitForSingleObject(thread, INFINITE) == WAIT_FAILED) ? -1 : 0
+	#define pthread_join(thread, value_ptr) (WaitForSingleObject(thread, INFINITE) == WAIT_FAILED) ? -1 : CloseHandle(thread) ? 0 : -1
 	#define pthread_exit(value_ptr) ExitThread(value_ptr)
 	#define pthread_mutex_t CRITICAL_SECTION
 	#define pthread_mutex_init(mutex, attr) InitializeCriticalSection(mutex)
 	#define pthread_mutex_lock(mutex) EnterCriticalSection(mutex)
-	#define pthread_mutex_trylock(mutex) TryEnterCriticalSection(mutex)
+	#define pthread_mutex_trylock(mutex) TryEnterCriticalSection(mutex) ? 0 : -1
 	#define pthread_mutex_unlock(mutex) LeaveCriticalSection(mutex)
+	#define pthread_mutex_destroy(mutex) DeleteCriticalSection(mutex)
 	#define pthread_cond_t CONDITION_VARIABLE
 	#define pthread_cond_init(cond, attr) InitializeConditionVariable(cond)
-	#define pthread_cond_wait(cond, mutex) SleepConditionVariableCS(cond, mutex, INFINITE)
-	#define pthread_cond_timedwait(cond, mutex, abstime) SleepConditionVariableCS(cond, mutex, abstime)
+	#define pthread_cond_wait(cond, mutex) SleepConditionVariableCS(cond, mutex, INFINITE) ? 0 : -1
+	#define pthread_cond_timedwait(cond, mutex, abstime) SleepConditionVariableCS(cond, mutex, abstime) ? 0 : -1
 	#define pthread_cond_signal(cond) WakeConditionVariable(cond)
 	#define pthread_cond_broadcast(cond) WakeAllConditionVariable(cond)
+	#define pthread_cond_destroy(cond)
 #else
 	#include <pthread.h>
 	#define thread_return void *
